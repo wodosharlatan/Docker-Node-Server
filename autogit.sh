7f08b67d102db4364f752a136dbdc29f770af0ac
Tomáš Bosák <tombos255@gmail.com>
2023-05-15 22:52:13 +0200

    Committing changes before pushing to GitHub and GitLab

--- autogit.sh
+++ autogit.sh
@@ -3,12 +3,11 @@ git commit -m "Committing changes before pushing to GitHub and GitLab"
 git pull origin git@github.com:wodosharlatan/Docker-Node-Server
 
 # Add remotes for GitHub and GitLab repositories
-git remote add git@github.com:wodosharlatan/Docker-Node-Server
-git remote add git@gitlab.com:wodosharlatan/Docker-Node-Server.git
+git remote add github git@github.com:wodosharlatan/Docker-Node-Server
+git remote add gitlab git@gitlab.com:wodosharlatan/Docker-Node-Server.git
 
 # Create a temporary branch
 git checkout -b temp
-
 
 git add .
 
@@ -18,14 +17,14 @@ git commit -m "[Auto GIT - $(date "+%d.%m.%Y")]"
 # Push changes to GitHub
 git push github temp:feature
 
-# Switch back to original branch
+# Switch back to the original branch
 git checkout feature
 
 # Merge changes from temp
 git merge temp
 
 # Push merged changes to GitLab
-git push gitlab git@github.com:wodosharlatan/Docker-Node-Server.git:feature
+git push gitlab feature
 
 # Clean up by deleting the temporary branch
 git branch -D temp
