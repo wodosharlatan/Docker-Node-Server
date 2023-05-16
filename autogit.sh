b82aa0218af3c0eae76c5efe900d22b9e7b9e4ea
Tomáš Bosák <tombos255@gmail.com>
2023-05-16 11:26:34 +0200

    [ Auto GIT - 16.05.2023 ]

--- autogit.sh
+++ autogit.sh
@@ -3,8 +3,9 @@ git commit -m "[ Auto GIT - $(date "+%d.%m.%Y") ]"
 git pull origin git@github.com:wodosharlatan/Docker-Node-Server
 
 # Add remotes for GitHub and GitLab repositories
-git remote add github git@github.com:wodosharlatan/Docker-Node-Server
 git remote add gitlab git@gitlab.com:wodosharlatan/Docker-Node-Server.git
+git remote add github git@github.com:wodosharlatan/Docker-Node-Server
+
 
 # Create a temporary branch
 git checkout -b temp
@@ -14,14 +15,17 @@ git add .
 # Create commit with date-based message
 git commit -m "[ Auto GIT - $(date "+%d.%m.%Y") ]"
 
+
+# Push merged changes to GitLab
+git push gitlab feature --force
+
 # Push changes to GitHub
 git push github temp:feature --force
 
 # Merge changes from temp
 git merge temp
 
-# Push merged changes to GitLab
-git push gitlab feature --force
+
 
 # Clean up by deleting the temporary branch
 git branch -D temp
